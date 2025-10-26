7e2b6fa799f26dac72aafe79fd4aeee4892b4e91
Julio cesar Cruz kubber <juliocesarcruzkubber@gmail.com>
2025-10-25 20:40:47 -0400

    Auth DTOs

---
+++ PlataformaIntegral.API/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PlataformaIntegral.API.DTOs.Auth
+{
+    public class RegisterDto
+    {
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int? IdTipoUsuario { get; set; }
+    }
+}
---
+++ PlataformaIntegral.API/DTOs/Auth/TokenResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PlataformaIntegral.API.DTOs.Auth
+{
+    public class TokenResponseDto
+    {
+        public string AccessToken { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
+        public DateTime AccessTokenExpiresAt { get; set; }
+
+    }
+}
